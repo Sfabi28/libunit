6c9370b8a74830090c0113a3d3d82e9e307165d3
elmondo <emondo1993@gmail.com>
2026-05-23 18:07:35 +0200

    filabustest

--- tests/memcpy/00_launcher.c
+++ tests/memcpy/00_launcher.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_launcher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sfabi <sfabi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: elmondo <elmondo@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 12:54:38 by elmondo           #+#    #+#             */
-/*   Updated: 2026/05/23 15:06:09 by sfabi            ###   ########.fr       */
+/*   Updated: 2026/05/23 18:00:57 by elmondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@
 int	test_memcpy_ok(void);
 int	test_memcpy_ko(void);
 int	test_memcpy_segv(void);
+int test_memcpy_sigbus(void);
 
-int	ft_memcpy_launcher(void)
+int ft_memcpy_launcher(void)
 {
 	t_unit_tests	*tests = NULL;
 
 	loadtest(&tests, "MEMCPY", "ok test", &test_memcpy_ok);
 	loadtest(&tests, "MEMCPY", "ko test", &test_memcpy_ko);
 	loadtest(&tests, "MEMCPY", "segv test", &test_memcpy_segv);
+	loadtest(&tests, "MEMCPY", "sigbus test", &test_memcpy_sigbus);
+
 	return (launchtest(&tests));
 }
--- tests/memcpy/03_segv_test.c
+++ tests/memcpy/03_segv_test.c
@@ -6,16 +6,16 @@
 /*   By: elmondo <elmondo@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 13:19:35 by elmondo           #+#    #+#             */
-/*   Updated: 2026/05/23 13:52:48 by elmondo          ###   ########.fr       */
+/*   Updated: 2026/05/23 17:58:11 by elmondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../tests.h"
 
-int	test_memcpy_segv(void)
+int test_memcpy_segv(void)
 {
-	char			src[6];
-	void *volatile	dst;
+	char src[6];
+	char *dst;
 
 	dst = NULL;
 	memcpy(dst, src, 6);
